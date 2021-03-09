bc4f840b289b84e26d6e32cba0742020981c157a
re041577 <57048111+alexsfelix@users.noreply.github.com>
2021-03-09 16:44:13 -0300

    validações
    incluindo validações para o formulário e mensagens de erros.

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -11,16 +11,16 @@ for (var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhvalido = true;
-    var alturaEhvalido = true;
+    var pesoEhvalido = validaPeso(peso);
+    var alturaEhvalido = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhvalido) {
         pesoEhvalido = false;
         tdImc.textContent = "Peso inválido!";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.0) {
+    if (!validaAltura) {
         alturaEhvalido = false;
         tdImc.textContent = "Altura inválida!";
         paciente.classList.add("paciente-invalido");
@@ -29,6 +29,22 @@ for (var i = 0; i < pacientes.length; i++) {
     if (alturaEhvalido && pesoEhvalido) {
         var imc = calculaImc(peso,altura);
         tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else {
+        return false;
     }
 }
 
--- js/form.js
+++ js/form.js
@@ -11,11 +11,29 @@ botaoAdicionar.addEventListener("click", function (event) {
     //Criar paciente
     var pacienteTr = criaTr(paciente);
 
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+    if (erros.length > 0) {
+        exibeMsgDeErros(erros);
+        return;
+    }
+
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
 
     form.reset(form);
+    document.querySelector("#msg-erro").innerHTML ="";
 });
+
+function exibeMsgDeErros(erros) {
+    var ul = document.querySelector("#msg-erro");
+    ul.innerHTML = "";
+    erros.forEach(function (erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form) {
     var paciente = {
@@ -25,29 +43,55 @@ function obtemPacienteDoFormulario(form) {
         gordura: form.gordura.value,
         imc: calculaImc(form.peso.value, form.altura.value)
     }
-    
+
     return paciente;
 }
 
-function criaTr(paciente){
+function criaTr(paciente) {
 
     var pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
 
-    pacienteTr.appendChild(criaTd(paciente.nome,"info-nome"));
-    pacienteTr.appendChild(criaTd(paciente.peso,"info-peso"));
-    pacienteTr.appendChild(criaTd(paciente.altura,"info-altura"));
-    pacienteTr.appendChild(criaTd(paciente.gordura,"info-gordura"));
-    pacienteTr.appendChild(criaTd(paciente.imc,"info-imc"));
+    pacienteTr.appendChild(criaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(criaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(criaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(criaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(criaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
 }
 
-function criaTd(dado, classe){
+function criaTd(dado, classe) {
 
     var td = document.createElement("td");
     td.textContent = dado;
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (!validaPeso(paciente.peso))
+        erros.push("Peso Inválido!");
+
+    if (!validaAltura(paciente.altura))
+        erros.push("Altura Inválido!");
+
+    if (paciente.nome.length <= 0)
+        erros.push("O campo Nome não pode estar em branco!");
+
+    if (paciente.peso.length <= 0)
+        erros.push("O campo Peso não pode estar em branco!");
+
+    if (paciente.altura.length <= 0)
+        erros.push("O campo Altura não pode estar em branco!");
+
+    if (paciente.gordura.length <= 0) {
+        erros.push("O campo Gordura não pode estar em branco!");
+    }
+
+    return erros;
 }
